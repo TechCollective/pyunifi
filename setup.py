9abc54b8daf84707b7543825aa557a5db0949321
Jeffrey Brite <jeff@techcollective.com>
2023-06-23 10:28:15 -0500

    more tests

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from setuptools import setup
 
 setup(name='pyunifi',
-      version='2.21',
-      description='API for Ubiquity Networks UniFi controller',
-      author='Caleb Dunn',
-      author_email='finish.06@gmail.com',
-      url='https://github.com/finish06/unifi-api',
-      packages=['pyunifi'],
-      scripts=['unifi-create-voucher', 'unifi-ls-clients'],
-      classifiers=[],
-      install_requires=['requests'],
-      )
+    version='2.21',
+    description='API for Ubiquity Networks UniFi controller',
+    author='Caleb Dunn',
+    author_email='finish.06@gmail.com',
+    url='https://github.com/finish06/unifi-api',
+    packages=['pyunifi'],
+    include_package_data=True,      
+    scripts=['unifi-create-voucher', 'unifi-ls-clients'],
+    classifiers=[],
+    install_requires=['requests'],
+    )
